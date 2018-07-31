354ff46d961d5d6f3ead9edcc65121521b0d4723
ticho <tibofolly@gmail.com>
2018-07-31 12:38:51 +0200

    form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
   def new
+    @errors = []
   end
 
   def create
+    user = User.create(
+      username: params[:username],
+      email: params[:email],
+      bio: params[:bio]
+    )
+    respond_to do |format|
+      if user.errors.any?
+        @errors = user.errors
+        format.html { render :new }
+      else
+        format.html { redirect_to root_path }
+      end
+    end
+  end
+
+  def show
+    if params[:id].to_i.to_s == params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = User.find_by(username: params[:id])
+    end
+  end
+
+  def all
+    @users = User.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: {in: 5..10}
+  validates :username, presence: true, length: {in: 2..10}
   validates :email, presence: true
   validates :bio, presence: true, length: {maximum: 500}
 end
