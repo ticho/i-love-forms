63d467aca3ebbeeb31521382451cb74e6663da7b
ticho <tibofolly@gmail.com>
2018-07-31 12:51:04 +0200

    added old funcs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/new' => 'users#new'
   post 'users/create' => 'users#create'
   get 'users' => 'users#all'
+  get 'users/:id' => 'users#show'
 
   root 'static_pages#index'
 end
